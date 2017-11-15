ec932c8d4cb008c12e334b25637617f5635163fa
Bryan <bryano1993@gmail.com>
2017-11-14 18:10:41 -0800

    step 1 complete, I will now merge to master

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('.artist').text(album.artist); // select the artist class and output the text content of the album object. The album object will then select the artist key and output its value.
+  $('#release-info').text(album.releaseInfo); // select the release-info id and output the text content of the album object. The album object will then select the releaseInfo key and output its value.
 }
